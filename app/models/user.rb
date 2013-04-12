4527594a075f96e09323c9bd3296112bff776ce4
Tom Wisniewski <t.wisniewski@hotmail.com>
2013-04-12 14:10:41 +0100

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :profile_name, presence: true
 has_many :statuses
 
   def full_name
