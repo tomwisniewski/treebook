cd26704f7f74d20983daf7617c76b2073c9720d1
Tom Wisniewski <t.wisniewski@hotmail.com>
2013-04-13 12:27:07 +0100

    done more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Treebook::Application.routes.draw do
+  get "profiles/show"
+
   devise_for :users
 
   devise_scope :user do
@@ -8,6 +10,7 @@ Treebook::Application.routes.draw do
   resources :statuses
   root to: 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
